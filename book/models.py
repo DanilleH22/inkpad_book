b2d6498c1e8556b1e1f9c0111391654966a1a0c3
DanilleH22 <>
2024-01-17 03:44:47 +0000

    Add CreateBook, Categories and CreateChapter Model

--- book/models.py
+++ book/models.py
@@ -1,3 +1,47 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+
+
+class Categories(models.Model):
+    genre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.genre
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+
+class CreateBook(models.Model):
+    title = models.CharField(max_length=300, unique=True)
+    slug = models.SlugField(max_length=300, unique=True,
+                            default='', blank=True)
+    excerpt = models.TextField(max_length=200)
+    biography = models.TextField(max_length=500, default='')
+    image = models.ImageField(upload_to='images/')
+    genre = models.ForeignKey(
+        Categories, related_name='category', on_delete=models.SET_NULL,
+        blank=True,
+        null=True,)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class CreateChapter(models.Model):
+    chapter_id = models.AutoField(primary_key=True)
+    chapter = models.CharField(max_length=200)
+    content = models.TextField()
+    status = models.IntegerField(
+        choices=((0, "Draft"), (1, "Published")), default=0)
