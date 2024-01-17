2f6168ea7b43f6efdfca0e8b252780de87ec5a71
DanilleH22 <>
2024-01-17 04:45:10 +0000

    Add Allauth

--- novelnest/settings.py
+++ novelnest/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-danilleh22-inkpad-book-kpti4fmcc6.us2.codeanyapp.com', '.herokuapp.com']
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'book',
+    'homepage',
 ]
 
 MIDDLEWARE = [
