0b4184cd60df6da32f82d5c3a82269da78882b6a
DanilleH22 <>
2024-01-17 03:45:19 +0000

    Add CreateBook, Categories and CreateChapter Model to admin

--- book/admin.py
+++ book/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import CreateBook, Categories, CreateChapter
 
 # Register your models here.
+admin.site.register(CreateBook)
+admin.site.register(Categories)
+admin.site.register(CreateChapter)
