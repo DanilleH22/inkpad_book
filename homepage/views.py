39a2eee1a3c3989152ea9e10c2567507be9d1336
DanilleH22 <>
2024-01-17 04:33:56 +0000

    Add quotes queryset to views

--- homepage/views.py
+++ homepage/views.py
@@ -7,5 +7,8 @@ from .models import FunFact, Quotes
 def home(request):
     # Retrieve three random fun facts from the database
     fun_facts = FunFact.objects.order_by('?')[:3]
-    
+
+    # Retrieve two random quotes from the database
+    quotations = Quotes.objects.order_by('?')[2]
+
     return render(request, 'home.html', {'fun_facts': fun_facts})
