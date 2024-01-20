f4e47b8df37e1cc291016ccf63c2f6e6707d12e9
Danille Hamilton <danilleham@hotmail.co.uk>
2024-01-20 13:35:45 +0000

    import settings and static from django

--- browse/urls.py
+++ browse/urls.py
@@ -1,8 +1,16 @@
 from . import views
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from .views import BookList
 
 
 urlpatterns = [
-    path('', views.BookList.as_view(), name='browse'),
+    path('', BookList.as_view(), name='browse'),
     path('<slug:slug>/', views.book_post, name='book_view'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
