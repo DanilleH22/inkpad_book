f2b2cf44e7d0f85f2d644024a91b6da913f8bfe5
Danille Hamilton <danilleham@hotmail.co.uk>
2024-01-23 22:49:43 +0000

    (browse/views.py):Add BookChapters view

--- browse/views.py
+++ browse/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
-from book.models import CreateBook
+from django.contrib.auth.mixins import LoginRequiredMixin
+from book.models import CreateBook, CreateChapter
 from django.views import generic
+from django.views.generic import View, DetailView, ListView, UpdateView
+# from book.forms import CreateChapterForm
 
 # Create your views here.
 
@@ -14,12 +17,47 @@ class BookList(generic.ListView):
 
 def book_post(request, slug):
     queryset = CreateBook.objects.filter()
-    book = get_object_or_404(CreateBook, slug=slug)
+    book_view = get_object_or_404(CreateBook, slug=slug)
 
     return render(
         request,
         "browse/book_view.html",
         {
-            "book": book,
+            "book_view": book_view,
         },
     )
+
+
+class BookChaptersView(generic.ListView):
+    model = CreateChapter
+    template_name = 'browse/view_chapter.html'
+
+    def get_queryset(self):
+        book_slug = self.kwargs.get('book_slug')
+        self.book = get_object_or_404(CreateBook, slug=book_slug)
+        return CreateChapter.objects.filter(book=self.book)
+
+    def get_context_data(self, **kwargs):
+        # Include the book in the context
+        context = super().get_context_data(**kwargs)
+        context['book'] = self.book
+        return context
+
+
+# class EditChapter(LoginRequiredMixin, UpdateView):
+#     template_name = 'book/edit_chapter.html'
+#     model = CreateChapter
+#     form_class = CreateChapterForm
+    # queryset = CreateChapter.objects.filter()
+
+
+    # def form_valid(self, form):
+    #     # Get book's slug from the URL
+    #     book_slug = self.kwargs['book_slug']
+    #     chapter_pk = self.kwargs['pk']
+    #     # Retrieve the book
+    #     book = get_object_or_404(CreateBook, slug=book_slug)
+    #     chapter = get_object_or_404(CreateChapter, pk=chapter_pk)
+    #     # Retrive the book chapter 
+
+    #     return super().form_valid(form)
