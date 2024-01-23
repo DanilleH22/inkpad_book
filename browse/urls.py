811959e28d96803af750d8c007d6dfebe725b530
Danille Hamilton <danilleham@hotmail.co.uk>
2024-01-23 23:38:20 +0000

    (browse/urls.py):Add EditChapter url path

--- browse/urls.py
+++ browse/urls.py
@@ -1,11 +1,11 @@
 from . import views
 from django.urls import path
-from .views import BookList, BookChaptersView
+from .views import BookList, BookChaptersView, EditChapter
 
 
 urlpatterns = [
     path('', BookList.as_view(), name='browse'),
     path('<slug:slug>/', views.book_post, name='book_view'),
     path('<slug:book_slug>/chapters', BookChaptersView.as_view(), name='view_book_chapters'),
-    # path('<slug:book_slug>/edit_chapter', EditChapter.as_view(), name='edit_book_chapter'),
+    path('<slug:book_slug>/edit_chapter/<int:pk>/', EditChapter.as_view(), name='edit_book_chapter'),
 ]
