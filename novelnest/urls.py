36a2a4a2e7aa7bb221aefa10700cd11f3556439a
Danille Hamilton <danilleham@hotmail.co.uk>
2024-01-23 22:48:51 +0000

    (novelnest/urls.py): Add if static for media

--- novelnest/urls.py
+++ novelnest/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('accounts/', include('allauth.urls')),
@@ -26,3 +28,7 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('', include('homepage.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
