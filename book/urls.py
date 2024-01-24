90aee92fc0345cca2aa1acd9da4b2448d95abd4c
Danille Hamilton <danilleham@hotmail.co.uk>
2024-01-24 02:06:33 +0000

    (book/urls.py): Add EditBook and DeleteBook paths

--- book/urls.py
+++ book/urls.py
@@ -1,6 +1,6 @@
 from . import views
 from django.urls import path
-from .views import AddBook, AddBookChapter, CompletedBook, DeleteChapter, EditChapter
+from .views import AddBook, AddBookChapter, CompletedBook, DeleteChapter, EditChapter, EditBookDetails, DeleteBook
 
 urlpatterns = [
     path('book_details/', AddBook.as_view(), name='book_details'),
@@ -9,8 +9,9 @@ urlpatterns = [
          CompletedBook.as_view(),
          name='completed_book'),
     path('<slug:book_slug>/edit_chapter/<int:pk>/', EditChapter.as_view(), name='edit_book_chapter'),
+    path('edit-book/<slug:slug>/<int:pk>/', EditBookDetails.as_view(), name='edit_book'),
     path('<slug:book_slug>/<int:pk>/delete', DeleteChapter.as_view(), name='delete_book_chapter'),
-    
+    path('<slug:slug>/delete', DeleteBook.as_view(), name='delete_book'),
 ]
 
 
