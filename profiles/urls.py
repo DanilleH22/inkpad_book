2fa3d7ffe7033e5e041ddc5808114a8f0560e181
Danille Hamilton <danilleham@hotmail.co.uk>
2024-01-24 23:10:49 +0000

    (profiles/urls.py): Add urls for bookmark

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,10 +2,11 @@ from . import views
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import view_profile
+from .views import view_profile, view_bookmark
 
 
 urlpatterns = [
     path('', view_profile, name='profile'),
+    path('bookmarked/', view_bookmark, name='bookmarked')
     
 ]
