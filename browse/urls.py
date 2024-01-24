ce922bf85ba38bd31f22e73ea4a1113a94a6b626
Danille Hamilton <danilleham@hotmail.co.uk>
2024-01-24 00:48:59 +0000

    (browse/urls.py):Remove EditChapter and DeleteChapter urls

--- browse/urls.py
+++ browse/urls.py
@@ -1,12 +1,10 @@
 from . import views
 from django.urls import path
-from .views import BookList, BookChaptersView, EditChapter, DeleteChapter
+from .views import BookList, BookChaptersView
 
 
 urlpatterns = [
     path('', BookList.as_view(), name='browse'),
     path('<slug:slug>/', views.book_post, name='book_view'),
-    path('<slug:book_slug>/chapters', BookChaptersView.as_view(), name='view_book_chapters'),
-    path('<slug:book_slug>/edit_chapter/<int:pk>/', EditChapter.as_view(), name='edit_book_chapter'),
-    path('<slug:book_slug>/<int:pk>/delete', DeleteChapter.as_view(), name='delete_book_chapter'),
+    path('<slug:book_slug>/chapters', BookChaptersView.as_view(), name='view_book_chapters'), 
 ]
