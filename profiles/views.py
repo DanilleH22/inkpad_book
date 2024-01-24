99c1d3adc83fd9f160621e6aead71179d9dc9652
Danille Hamilton <danilleham@hotmail.co.uk>
2024-01-24 00:47:04 +0000

    (profiles/views.py): Add EditChapter and DeleteChapter

--- profiles/views.py
+++ profiles/views.py
@@ -1,23 +1,12 @@
 from django.shortcuts import render, get_object_or_404
-from book.models import CreateBook
+from book.models import CreateBook, CreateChapter
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import View, DetailView, ListView, UpdateView, DeleteView
+from django.urls import reverse_lazy, reverse
+from django.contrib import messages
 
 # Create your views here.
 
 def view_profile(request):
     user_books = request.user.books.filter(author=request.user)
     return render(request, 'profiles/profile.html', {'user_books': user_books})
-
-
-
-# def edit_profile(request):
-#     profile = request.user.profile
-
-#     if request.method == 'POST':
-#         form = ProfileForm(request.POST, instance=profile)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('profile')
-#     else:
-#         form = ProfileForm(instance=profile)
-
-#     return render(request, 'edit_profile.html', {'form': form})
