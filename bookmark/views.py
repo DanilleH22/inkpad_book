69ee073e80f79b3fa4a66a3f3ac466b74a7e7c99
Danille Hamilton <danilleham@hotmail.co.uk>
2024-01-24 15:02:02 +0000

    (bookmark/views.py):Change if return to redirect to current page

--- bookmark/views.py
+++ bookmark/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Bookmark
+from django.urls import reverse_lazy, reverse
+from book.models import CreateBook
+from django.http import HttpResponseRedirect
 # Create your views here.
+
+@login_required
+def bookmark_book(request, slug):
+    # Get the book using its slug
+    book = get_object_or_404(CreateBook, slug=request.POST.get('book_slug'))
+    # 
+    bookmark, created = Bookmark.objects.get_or_create(
+        user=request.user,
+        book=book
+    )
+    # book.bookmark.add(request.user)
+    return HttpResponseRedirect(reverse('browse', args=[str(slug)]))
