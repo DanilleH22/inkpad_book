9489a4776cbb675aa23d0c428e25a8a5eaa26210
Danille Hamilton <danilleham@hotmail.co.uk>
2024-01-24 23:08:42 +0000

    (profiles/views.py): Add view bookmarks function

--- profiles/views.py
+++ profiles/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from book.models import CreateBook, CreateChapter
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import View, DetailView, ListView, UpdateView, DeleteView
-from django.urls import reverse_lazy, reverse
-from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 def view_profile(request):
     user_books = request.user.books.filter(author=request.user)
     return render(request, 'profiles/profile.html', {'user_books': user_books})
+
+
+@login_required
+def view_wishlist(request):
+    bookmarked_books = request.user.bookmarked_books.all()
+
+    return render(request, "profiles/bookmarked.html", {'bookmarked_books': bookmarked_books})
