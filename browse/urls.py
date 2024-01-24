64803547c8e0be439cef51deacc5454010245caa
Danille Hamilton <danilleham@hotmail.co.uk>
2024-01-24 19:17:35 +0000

    (browse/urls.py): Add Bookmark View urls

--- browse/urls.py
+++ browse/urls.py
@@ -1,10 +1,11 @@
 from . import views
 from django.urls import path
-from .views import BookList, BookChaptersView
+from .views import BookList, BookChaptersView, BookmarkView
 
 
 urlpatterns = [
     path('', BookList.as_view(), name='browse'),
     path('<slug:slug>/', views.book_post, name='book_view'),
     path('<slug:book_slug>/chapters', BookChaptersView.as_view(), name='view_book_chapters'), 
+    path('<slug:slug>/bookmark/', BookmarkView, name='bookmark_book'),
 ]
