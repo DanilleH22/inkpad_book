f11f951c5e43efb26ffe867474cdd3c6b80842c7
Danille Hamilton <danilleham@hotmail.co.uk>
2024-01-25 00:29:19 +0000

    (profiles/views.py): Add book draft function

--- profiles/views.py
+++ profiles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from book.models import CreateBook, CreateChapter
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
@@ -6,8 +6,20 @@ from django.contrib.auth.models import User
 # Create your views here.
 
 def view_profile(request):
-    user_books = request.user.books.filter(author=request.user)
-    return render(request, 'profiles/profile.html', {'user_books': user_books})
+    user_books = request.user.books.filter(author=request.user).exclude(status=0)
+    drafts = request.user.books.filter(author=request.user).exclude(status=1)
+    return render(
+        request, 
+        'profiles/profile.html', {
+            'user_books': user_books,
+            'drafts': drafts,
+            }
+            )
+
+
+def book_draft(request, slug):
+    view_drafts = get_object_or_404(CreateBook, slug=slug, status=0)
+    return render(request, 'profiles/draft.html', {'view_drafts': view_drafts})
 
 
 @login_required
