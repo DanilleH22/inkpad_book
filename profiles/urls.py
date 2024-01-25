75a34ce0d27c8268819d7fde3efb232eac31d925
Danille Hamilton <danilleham@hotmail.co.uk>
2024-01-25 00:29:49 +0000

    (profiles/urls.py): Add book draft url path

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,11 +2,12 @@ from . import views
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import view_profile, view_bookmark
+from .views import view_profile, view_bookmark, book_draft
 
 
 urlpatterns = [
     path('', view_profile, name='profile'),
-    path('bookmarked/', view_bookmark, name='bookmarked')
+    path('bookmarked/', view_bookmark, name='bookmarked'),
+    path('<slug:slug>/draft/', book_draft, name='draft'),
     
 ]
