7e034cb02473fced1ebf4b6851caed5d3070eee5
Danille Hamilton <danilleham@hotmail.co.uk>
2024-01-26 23:16:49 +0000

    (profiles/urls.py): Change draft url order

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,6 +6,6 @@ from .views import view_profile, view_bookmark, book_draft
 urlpatterns = [
     path('', view_profile, name='profile'),
     path('bookmarked/', view_bookmark, name='bookmarked'),
-    path('<slug:slug>/draft/', book_draft, name='draft'),
+    path('draft/<slug:slug>/<int:pk>/', book_draft, name='draft'),
     
 ]
