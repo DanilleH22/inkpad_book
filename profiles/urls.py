5a224abb675c610b5cfb72a66d561ddd645a77ee
Danille Hamilton <danilleham@hotmail.co.uk>
2024-01-26 11:51:48 +0000

    Remoove media path

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,5 @@
 from . import views
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from .views import view_profile, view_bookmark, book_draft
 
 
