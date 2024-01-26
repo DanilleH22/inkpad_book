55ee4d3b7c7cdf4694db6b44c3bdd2cfd1e5c657
Danille Hamilton <danilleham@hotmail.co.uk>
2024-01-26 00:17:50 +0000

    (book/forms.py): Add widgets and exclude books for CreateChapter

--- book/forms.py
+++ book/forms.py
@@ -7,11 +7,23 @@ class CreateBookForm(forms.ModelForm):
         model = CreateBook
         fields = ["title", "genre", 'status', 'excerpt',
                   'synopsis', 'image']
+        widgets = {
+            "title": forms.TextInput(attrs={'class': 'form-control'}),
+            "genre": forms.Select(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+            'excerpt': forms.Textarea(attrs={'class': 'form-control'}),
+            'synopsis': forms.Textarea(attrs={'class': 'form-control'}),
+        }
 
 
 class CreateChapterForm(forms.ModelForm):
-    book = forms.ModelChoiceField(queryset=CreateBook.objects.all(), required=True)
 
     class Meta:
         model = CreateChapter
         fields = ['chapter', 'content', 'status']
+        widgets = {
+            "chapter": forms.TextInput(attrs={'class': 'form-control'}),
+            'status': forms.Select(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control'}),
+        }
+        exclude = ['book']
