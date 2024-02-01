5d65a9c829a0981e7bedcc6d93b52d32a53a8379
Danille Hamilton <danilleham@hotmail.co.uk>
2024-02-01 20:10:58 +0000

    Remove profile url & add published-books

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,11 +1,12 @@
 from . import views
 from django.urls import path
-from .views import view_profile, view_bookmark, book_draft
+from .views import view_published, view_bookmark, book_draft
 
 
 urlpatterns = [
-    path('', view_profile, name='profile'),
+
     path('bookmarked/', view_bookmark, name='bookmarked'),
     path('draft/<slug:slug>/<int:pk>/', book_draft, name='draft'),
+    path('published-books/', view_published, name='published-books'),
     
 ]
