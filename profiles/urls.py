0691bdc2f97ebc3a5265e510ba5142491114f401
Danille Hamilton <danilleham@hotmail.co.uk>
2024-02-01 20:35:46 +0000

    Add list of drafts view

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,6 @@
 from . import views
 from django.urls import path
-from .views import view_published, view_bookmark, book_draft
+from .views import view_published, view_bookmark, book_draft, view_drafts
 
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('bookmarked/', view_bookmark, name='bookmarked'),
     path('draft/<slug:slug>/<int:pk>/', book_draft, name='draft'),
     path('published-books/', view_published, name='published-books'),
+    path('draft-books/', view_drafts, name='draft-books'),
     
 ]
