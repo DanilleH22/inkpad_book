9ec10a19a15750daac6cf8b439ffc4de5e32eff8
Danille Hamilton <danilleham@hotmail.co.uk>
2024-02-04 15:40:30 +0000

    Add render for external hosts

--- novelnest/settings.py
+++ novelnest/settings.py
@@ -36,6 +36,11 @@ ALLOWED_HOSTS = [
     '8000-danilleh22-inkpadbook-tdxhkgcyxg4.ws-eu107.gitpod.io', '.herokuapp.com']
 
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+
 # Application definition
 
 INSTALLED_APPS = [
